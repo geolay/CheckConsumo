7c9e2a2fd75ded99a4279ffe3875bdf92018705e
ernes <ernesto.olavarria@gmail.com>
2021-01-31 06:30:07 -0300

    Compila bien, pero algo falla en el celular

--- app/src/main/java/com/example/checkconsumo/FirstFragment.kt
+++ app/src/main/java/com/example/checkconsumo/FirstFragment.kt
@@ -1,31 +1,55 @@
 package com.example.checkconsumo
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.checkconsumo.databinding.FragmentFirstBinding
+import com.example.checkconsumo.model.ConViewmodel
+import com.example.checkconsumo.model.ConsumAdapter
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+    private lateinit var binding: FragmentFirstBinding
+    private val viewModel : ConViewmodel by activityViewModels()
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
-    }
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+         }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        var adapter = ConsumAdapter()
+        binding.rv.adapter = adapter
+        binding.rv.layoutManager = LinearLayoutManager(context)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+        viewModel.allConsumption.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.update(it)
+            }
+        })
+
+        binding.fab.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+
+        adapter.selectedItem().observe(viewLifecycleOwner, Observer {
+            it?.let {
+                viewModel.selected(it)
+                findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            }
+        })
     }
 }
